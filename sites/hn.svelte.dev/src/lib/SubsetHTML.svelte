0ad576eae141a329c2077999b6f698582203df2f
GrygrFlzr <git@cybeast.dev>
2026-04-07 23:50:15 +0700

    feat: rewrite hn /item?id= links

--- sites/hn.svelte.dev/src/lib/SubsetHTML.svelte
+++ sites/hn.svelte.dev/src/lib/SubsetHTML.svelte
@@ -1,17 +1,45 @@
 <script lang="ts">
 	import { parse, type Inline } from './htmlSubsetParse';
-
+	import { resolve } from '$app/paths';
 	const { content }: { content: string } = $props();
 
 	const parsedContent = $derived(parse(content));
+
+	type PossiblyRewrittenUrl = {
+		rel: 'external' | undefined;
+		href: string;
+	};
+	const HN_HOSTNAME = 'news.ycombinator.com' as const;
+	function tryRewriteLink(originalUrl: URL): PossiblyRewrittenUrl {
+		const { hostname, pathname, searchParams, hash, href } = originalUrl;
+		if (hostname === HN_HOSTNAME && pathname === '/item') {
+			// for the purposes of typescript control flow,
+			// .has() does not impact .get()
+			const itemId = searchParams.get('id');
+			if (itemId) {
+				// only support rewriting /item?id= links
+				// which actually covers most legitimate uses
+				return {
+					href: `${resolve('/item/[id=numeric]', {
+						id: itemId as `${number}`
+					})}${hash}`,
+					rel: undefined
+				};
+			}
+		}
+		// otherwise spit back original
+		return { href, rel: 'external' };
+	}
 </script>
 
 {#snippet inline(child: Inline)}
 	{#if child.type === 'text'}
 		{child.text}
 	{:else if child.type === 'link'}
-		{#if ['http:', 'https:'].includes(new URL(child.href).protocol)}
-			<a rel="external" href={child.href}>{child.text}</a>
+		{@const originalUrl = new URL(child.href)}
+		{#if ['http:', 'https:'].includes(originalUrl.protocol)}
+			{@const { href, rel } = tryRewriteLink(originalUrl)}
+			<a {href} {rel}>{child.text}</a>
 		{:else}
 			<!-- link might be other protocol like `javascript:` so bail out and do best effort render -->
 			<del>{child.text}</del>
